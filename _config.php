6ec75e9db0f1571436e8a50d37f9386aeb914db5
= <vinnie_watson@hotmail.com>
2020-09-18 12:51:42 +1200

    udate yml classes

--- _config.php
+++ _config.php
@@ -1,6 +1,8 @@
 <?php
 
 use SilverStripe\ORM\FieldType\DBField;
+use SilverStripe\Core\Manifest\ClassLoader;
+use SilverStripe\Admin\CMSMenu;
 
 if (!class_exists("Faker\Generator")) {
     throw new RuntimeException("The silverstripe-mock-dataobjects module requires the Faker PHP library. You can install it by running 'composer require fzaninotto/faker:1.1.*@dev' in your web root. If you installed this module via composer, make the directory fzaninotto/faker exists in vendor/.");
@@ -10,7 +12,7 @@ define('MOCK_DATAOBJECTS_DIR', basename(dirname(__FILE__)));
 
 DBField::add_extension('MockDBField');
 
-foreach (SS_ClassLoader::instance()->getManifest()->getDescendantsOf(DBField::class) as $class) {
+foreach (ClassLoader::instance()->getManifest()->getDescendants(DBField::class) as $class) {
     $mockClass = "Mock{$class}Field";
     if (class_exists($mockClass)) {
         $class::add_extension($mockClass);
